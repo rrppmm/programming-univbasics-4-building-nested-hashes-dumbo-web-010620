17f3cd8d3a7c85104d4eef4bf3582850974719f5
Paul M <paulmcadory@gmail.com>
2019-12-31 19:40:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,7 @@ def monopoly_with_fourth_tier
 	        "mortgage_value" => "$400"
 	      },
 	      :shortline_railroad => {
-	        "mortgage value" => []
+	        "mortgage value" => "$800"
 	      }
 	    }
 	  }

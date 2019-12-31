38caa7e67cfb5cb5e5b6bc92e407db0180aed5f5
Paul M <paulmcadory@gmail.com>
2019-12-31 19:25:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
 	    :names => {
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
-	      :key7 => {},
+	      :b_and_o_railroad => {},
 	      :key8 => {}
 	    }
 	  }
